19c6825386d1236f65444f7e886502b94e74868c
hrishikesh kamble <hrishikamble13@gmail.com>
2022-10-20 22:02:27 +0530

    welcome message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day14StackAndQueue
+{
+    public class Program
+    {
+        public static void Main(string[]args)
+        {
+            Console.WriteLine("welcome to Stack And Queue problem");
+        }
+    }
+}
